f9b89c38d04b7a6778c8c10431f20c8f57604854
Mykhailo <mikesulimko@gmail.com>
2026-01-13 09:15:16 +0000

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogModule } from 'primeng/dialog';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
+imports: [
+  BrowserModule,
+  AppRoutingModule,
+  FormsModule,
+  BrowserAnimationsModule,
+  DialogModule
 ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  
+}
 
 
 
