83ac5b967f8fe93ae5bab55d86dc2fcf45d42721
Nikita Vyshchypan <deathkitty234@gmail.com>
2026-01-14 10:01:49 +0000

    no message

--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-
+import { DialogModule } from 'primeng/dialog';
 import { UsersComponent } from './users.component';
 import { UsersRoutingModule } from './users-routing.module';
 
 @NgModule({
+  
   
   imports: [
     CommonModule,
     FormsModule,
     ButtonModule,
     UsersRoutingModule,
+    DialogModule,
+    
   ]
 })
 export class UsersModule {
