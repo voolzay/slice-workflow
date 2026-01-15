a1217698a8a694ca7f79a8ecd50e7b50d665b793
Nikita Vyshchypan <deathkitty234@gmail.com>
2026-01-15 09:18:51 +0000

    no message

---
+++ src/app/pages/tickets/tickets.component.ts
@@ -0,0 +1,26 @@
+// =================================================
+// # IMPORTS ANGULAR
+// =================================================
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+
+// =================================================
+// # IMPORTS PRIMENG
+// =================================================
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { DialogModule } from 'primeng/dialog';
+import { ConfirmationService, MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-tickets',
+  templateUrl: './tickets.component.html',
+  styleUrls: ['./tickets.component.scss']
+})
+export class TicketsComponent {
+
+}
