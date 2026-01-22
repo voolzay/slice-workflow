4e3e82a6aea58c9d58c79635f5b95ffd65f8a777
Nikita Vyshchypan <deathkitty234@gmail.com>
2026-01-22 15:17:49 +0000

    Merge branch 'main' of https://github.com/voolzay/slice-workflow
    # Conflicts:
    #	src/app/services/workflow.service.ts

---
+++ src/app/pages/slice-workflow/task-edit/task-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-edit',
+  templateUrl: './task-edit.component.html',
+  styleUrl: './task-edit.component.scss'
+})
+export class TaskEditComponent {
+
+}
