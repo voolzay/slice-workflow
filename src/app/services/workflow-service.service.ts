a59e28b5fd819259408ebb2ad0c0a420bd4c9f43
Nikita Vyshchypan <deathkitty234@gmail.com>
2026-01-26 09:18:40 +0000

    no message

--- src/app/services/workflow-service.service.ts
+++ src/app/services/workflow-service.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { WorkflowList } from '../models/workflow-list.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkflowService {
+  createList(list: WorkflowList) {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 
